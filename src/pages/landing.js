c540ff0acc93178e46b912164dafca9a42991604
MaxFerney <ferneym2@winthrop.edu>
2021-04-07 19:44:27 -0400

    changed header - a lil bit of content

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -7,8 +7,9 @@ function Home() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Max's <code>React</code> Website
         </p>
+        <p>Currently Under Construction...</p>
         <a
           className="App-link"
           href="https://reactjs.org"
